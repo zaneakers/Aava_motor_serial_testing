0d06ddd073d8f7222a614adfc9e7632da674231d
Zane Akers <pugnat2020@gmail.com>
2025-05-15 14:35:39 -0500

    refactor data reading and parsing logic in serial communication script

--- serialscript1.jl
+++ serialscript1.jl
@@ -17,24 +17,31 @@ println(readline(sp))
 ##create containers to hold input (read) data
 ADC_data = Float8[]
 Positional_data = Int8[]
-time = Int8[0] 
-##read all data in input buffer
-converted_data = parse(String, nonblocking_read(sp))  # Converts Vector{UInt8} byte array to string
+time = Float64[0.0] 
+
+bytes_waiting = sp_input_waiting(sp) #check how many bytes are waiting in the input buffer
+while bytes_waiting > 0
+    println("Found $bytes_waiting bytes in buffer")
+    buffer = Vector{UInt8}(undef, bytes_waiting)
+    bytes_read = readbytes!(sp, buffer, bytes_waiting)
+    converted_data = String(buffer[1:bytes_read])
 
 ## divide data into positional and ADC data ##
-for m in eachmatch(r"\d+\.\d+|\d+", converted_data) 
-    #matches floats first then ints, regex ignores spaces
-    
-    if occursin(r"\d+\.\d+", m.match) #check if match is a float 
-        push!(ADC_data, parse(Float8, m.match))
+    for m in eachmatch(r"\d+\.\d+|\d+", converted_data) 
+        #matches floats first then ints, regex ignores spaces
         
-    elseif occursin(r"\d+", m.match) #check if match is an int
-        push!(Positional_data,parse(Int8, m.match))
-    else
-        println("match not working")
+        if occursin(r"\d+\.\d+", m.match) #check if match is a float 
+            push!(ADC_data, parse(Float8, m.match))
+            
+        elseif occursin(r"\d+", m.match) #check if match is an int
+            push!(Positional_data,parse(Int8, m.match))
+        else
+            println("match not working")
+        end
+        push!(time, time[end] + 0.05) #increment time by 0.00005 sec or 0.05 ms
     end
-    push!(time, time[end] + 0.05) #increment time by 0.00005 sec or 0.05 ms
-end
+##read all data in input buffer
+
 
 df = DataFrame(Current=ADC_data, Position=Positional_data, Time = time)
 plotcurrent = plot(df.Time, df.Current, label="Current", xlabel="Time (ms)", ylabel="Current", title="Current vs Time")
