c3408284a4f6f3caf7e42cabecdc277b6120d954
Zane Akers <pugnat2020@gmail.com>
2025-05-15 16:13:31 -0500

    fix: correct data types and improve comments in serial communication script

--- serialscript1.jl
+++ serialscript1.jl
@@ -1,13 +1,18 @@
-using LibSerialPort, Dataframes, CSV, Plots
+using LibSerialPort, DataFrames, CSV, Plots
+
+
+
+### This script is used to read data from a serial port and parse it into a DataFrame
 
 list_ports()
 
 sp = LibSerialPort.open("COM1", 115200) 
-#allows configuring data framing parameters like ndatabits, parity, and nstopbits.
+#open allows configuring data framing parameters like ndatabits, parity, and nstopbits.
 # deafult is 8N1
 
-##clear both buffers, use default flow control
+##clear both buffers, use default(no flow control)
 set_flow_control(sp)
+#flush input and output buffers
 sp_flush(sp, SP_BUF_BOTH)
 
 ## Test: write and immediately blocking_read a full line ending w newline char from input buffer
@@ -15,37 +20,49 @@ write(sp, "hello\n")
 println(readline(sp))
 
 ##create containers to hold input (read) data
-ADC_data = Float8[]
-Positional_data = Int8[]
-time = Float64[0.0] 
+ADC_data = Float16[]
+Positional_data = UInt16[]
+time = Float16[0.0] 
 
-bytes_waiting = sp_input_waiting(sp) #check how many bytes are waiting in the input buffer
+##create plots
+plotcurrent = plot(title="Real Time Current vs Time",xlabel="Time (ms)", 
+                ylabel="Current",
+                label="Current", legend=:topright)
+plotposition = plot(title="Position vs Time", xlabel="Time (ms)", 
+                ylabel="Position", 
+                label="Position")
+### find number of bytes in input buffer, create a new buffer, and read bytes into it
+
+bytes_waiting = sp_input_waiting(sp) 
+buffer = Vector{UInt8}(undef, bytes_waiting)
 while bytes_waiting > 0
     println("Found $bytes_waiting bytes in buffer")
-    buffer = Vector{UInt8}(undef, bytes_waiting)
     bytes_read = readbytes!(sp, buffer, bytes_waiting)
     converted_data = String(buffer[1:bytes_read])
 
-## divide data into positional and ADC data ##
+    ## divide data into positional and ADC data ##
     for m in eachmatch(r"\d+\.\d+|\d+", converted_data) 
-        #matches floats first then ints, regex ignores spaces
+    #matches floats first then ints, regex ignores spaces
         
         if occursin(r"\d+\.\d+", m.match) #check if match is a float 
-            push!(ADC_data, parse(Float8, m.match))
+            push!(ADC_data, parse(Float16, m.match))
             
         elseif occursin(r"\d+", m.match) #check if match is an int
-            push!(Positional_data,parse(Int8, m.match))
+            push!(Positional_data,parse(UInt16, m.match))
         else
             println("match not working")
         end
-        push!(time, time[end] + 0.05) #increment time by 0.00005 sec or 0.05 ms
+        push!(time, time[end] + 0.05) #increment time by 0.05 ms for every match
     end
-##read all data in input buffer
-
+    bytes_waiting = sp_input_waiting(sp)
+    plot!(plotcurrent, time, ADC_data, overwrite=true)
+    display(plotcurrent)  
+    plot!(plotposition, time, Positional_data, overwrite=true)
+    display(plotposition)
+end
 
 df = DataFrame(Current=ADC_data, Position=Positional_data, Time = time)
-plotcurrent = plot(df.Time, df.Current, label="Current", xlabel="Time (ms)", ylabel="Current", title="Current vs Time")
-plotposition = plot(df.Time, df.Position, label="Position", xlabel="Time (ms)", ylabel="Position", title="Position vs Time")
+
 CSV.write("motor_pos_adc.csv", df)
 
 close(sp)
