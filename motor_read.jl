2c71c40843b41a4cd5f796fd1908518a6e7c516d
Zane Akers <pugnat2020@gmail.com>
2025-05-16 13:13:39 -0500

    adding command lines

--- motor_read.jl
+++ motor_read.jl
@@ -3,5 +3,15 @@ using LibSerialPort
 #will integrate into motor_write_read.jl
 #assume both buffers are cleared
 
-write(sp, "mstop\n")
-#need to know parameters for each command
+
+println("Enter a command:")
+command = readline()
+if command=="mstop"
+    println("Enter a value for mstop:")
+    x = parse(Int8, readline())
+    write(sp, "mstop($x)\n")
+elseif command=="mslow"
+    println("Enter position to start deaceleration:")
+    x = parse(Int8, readline())
+    write(sp, "mslow($x)\n")
+
