2a25528dd025bbd80387021beb96d2eb282dbe9a
Zane Akers <pugnat2020@gmail.com>
2025-05-16 14:05:59 -0500

    added if statements for commands to motor_read, deleted unnecessary files

--- motor_read.jl
+++ motor_read.jl
@@ -2,16 +2,27 @@ using LibSerialPort
 
 #will integrate into motor_write_read.jl
 #assume both buffers are cleared
-
+##be able to loop through a range of parameters for each command
 
 println("Enter a command:")
 command = readline()
 if command=="mstop"
-    println("Enter a value for mstop:")
+    println("Enter a range of values for mstop:")
     x = parse(Int8, readline())
     write(sp, "mstop($x)\n")
 elseif command=="mslow"
     println("Enter position to start deaceleration:")
     x = parse(Int8, readline())
     write(sp, "mslow($x)\n")
+elseif command=="mtime"
+    println("Enter desired rise time")
+    x = parse(Int8, readline())
+    write(sp, "mtime($x)\n")
+elseif command=="sat"
+    println("Enter desired decel saturation point:")
+    x = parse(Int8, readline())
+    write(sp, "sat($x)\n")
+
+end
+
 
